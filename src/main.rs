422f22a8aec07f48e5f120eb1ca0ae7d27e5f2b9
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2018-01-27 01:59:31 -0600

    got basic sanitize text function working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate csv;
+#[macro_use] extern crate lazy_static;
+extern crate regex;
 #[macro_use]
 extern crate serde_derive;
 use std::error::Error;
-use std::process;
+use std::borrow::Cow;
 use std::fs::File;
+use regex::Regex;
+use regex::Captures;
 
 #[derive(Debug, Deserialize)]
 struct Record {
@@ -21,19 +25,40 @@ fn read_csv(file_path: &str) -> Result<Vec<Record>, Box<Error>> {
     let file = File::open(file_path)?;
     let mut records = vec![];
     let mut rdr = csv::Reader::from_reader(file);
+    let mut i = 0;
+    let lim = 3;
     for result in rdr.deserialize() {
+        if i > lim { break }
         let record: Record = result?;
         records.push(record);
+        i += 1;
     }
     Ok(records)
 }
 
+fn sanitize_text(text: &str) -> Cow<str> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(\s+)|(\p{P}+)").unwrap();
+    }
+    RE.replace_all(text, |captures: &Captures| {
+        if captures.get(1).is_some() {
+            " ".into()
+        } else if captures.get(2).is_some() {
+            "".into()
+        } else {
+            unreachable!("Unknown group matched")
+        }
+    })
+}
+
 fn main() {
     let train_path = "data/train.csv";
-    if let Err(err) = read_csv(&train_path) {
-        println!("error reading train file: {}", err);
-        process::exit(1);
-    }
+    let train = match read_csv(&train_path) {
+        Ok(rs) => rs,
+        Err(e) => panic!("error parsing training file: {}", e),
+    };
+    println!("{:?}", &train[1].comment_text);
+    println!("{:?}", sanitize_text(&train[1].comment_text));
 }
 
 
